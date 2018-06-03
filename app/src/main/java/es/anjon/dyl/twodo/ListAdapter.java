4166311bb88fde8a57d65ce559373d1f383b11b8
Dylan Jones <dyl@anjon.es>
2018-06-03 21:59:51 +0100

    Add checkbox to list items

--- app/src/main/java/es/anjon/dyl/twodo/ListAdapter.java
+++ app/src/main/java/es/anjon/dyl/twodo/ListAdapter.java
@@ -2,7 +2,9 @@ package es.anjon.dyl.twodo;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -11,17 +13,16 @@ import java.util.List;
 import es.anjon.dyl.twodo.models.ListItem;
 
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
+
     private List<ListItem> mListItems;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public LinearLayout mTextView;
-        public ViewHolder(LinearLayout v) {
+        public TextView mTitleView;
+        public CheckBox mCheckedView;
+        public ViewHolder(View v) {
             super(v);
-            mTextView = v;
+            mTitleView = v.findViewById(R.id.list_item_title);
+            mCheckedView = v.findViewById(R.id.list_item_checkbox);
         }
     }
 
@@ -29,27 +30,20 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
         mListItems = myDataset;
     }
 
-    // Create new views (invoked by the layout manager)
     @Override
     public ListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        // create a new view
         LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.list_item, parent, false);
-        ViewHolder vh = new ViewHolder(v);
-        return vh;
+        return new ViewHolder(v);
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        TextView tv = (TextView) holder.mTextView.findViewById(R.id.list_item_title);
-        tv.setText(mListItems.get(position).getTitle());
-
+        ListItem item = mListItems.get(position);
+        holder.mTitleView.setText(item.getTitle());
+        holder.mCheckedView.setChecked(item.getChecked());
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return mListItems.size();
