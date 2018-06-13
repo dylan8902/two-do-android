bfbc8b3b8862237d6029fcf9ace3b99e025c5838
Dylan Jones <dyl@anjon.es>
2018-06-13 07:53:59 +0100

    #8 Adds edit button and dialog with delete button
    Fixes #9

--- app/src/main/java/es/anjon/dyl/twodo/ListAdapter.java
+++ app/src/main/java/es/anjon/dyl/twodo/ListAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
@@ -17,6 +18,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
 
     private List<ListItem> mListItems;
     private OnItemCheckedListener mListener;
+    private OnEditItemClickedListener mEditItemListener;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView mTitleView;
@@ -25,9 +27,11 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
         public TextView mDueDateView;
         public TextView mDueMonthView;
         public LinearLayout mCalendarView;
+        public Button mEditButton;
         private OnItemCheckedListener mListener;
+        private OnEditItemClickedListener mEditItemListener;
 
-        public ViewHolder(View v, OnItemCheckedListener listener) {
+        public ViewHolder(View v, OnItemCheckedListener listener, OnEditItemClickedListener editItemListener) {
             super(v);
             mTitleView = v.findViewById(R.id.list_item_title);
             mPriorityView = v.findViewById(R.id.list_item_priority);
@@ -35,20 +39,23 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
             mDueDateView = v.findViewById(R.id.list_item_due_date);
             mDueMonthView = v.findViewById(R.id.list_item_due_month);
             mCalendarView = v.findViewById(R.id.list_item_calendar);
+            mEditButton = v.findViewById(R.id.list_item_edit);
             mListener = listener;
+            mEditItemListener = editItemListener;
         }
     }
 
-    public ListAdapter(List<ListItem> myDataset, OnItemCheckedListener listener) {
+    public ListAdapter(List<ListItem> myDataset, OnItemCheckedListener listener, OnEditItemClickedListener editItemListener) {
         mListItems = myDataset;
         mListener = listener;
+        mEditItemListener = editItemListener;
     }
 
     @Override
     public ListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.list_item, parent, false);
-        return new ViewHolder(v, mListener);
+        return new ViewHolder(v, mListener, mEditItemListener);
     }
 
     @Override
@@ -72,6 +79,12 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
                 holder.mListener.onItemChecked(holder.getAdapterPosition());
             }
         });
+        holder.mEditButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.mEditItemListener.onEditItemClicked(holder.getAdapterPosition());
+            }
+        });
     }
 
     @Override
@@ -80,7 +93,11 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     }
 
     public interface OnItemCheckedListener {
-        void onItemChecked(int item);
+        void onItemChecked(int position);
+    }
+
+    public interface OnEditItemClickedListener {
+        void onEditItemClicked(int position);
     }
 
 }
